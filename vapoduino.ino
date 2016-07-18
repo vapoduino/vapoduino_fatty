0014fc2139088b8d26cbea4d5c5c7feafa020c0a
Benedikt Schlagberger <schlagbe@in.tum.de>
2016-07-18 23:18:31 +0200

    addign pid change via serial commands

--- vapoduino.ino
+++ vapoduino.ino
@@ -30,9 +30,9 @@
 #define BT_RX_PIN 8
 #define BT_TX_PIN 7
 
-#define PID_P 11          // counter-"force"
-#define PID_I 6        // counter-offset
-#define PID_D 8          // dampen controlling, react on fast changes
+#define PID_P 19 // counter-"force" http://www.ascii-code.com/
+#define PID_I 12 // counter-offset
+#define PID_D 12 // dampen controlling, react on fast changes
 #define PID_P_HEATING 3.6
 #define PID_I_HEATING 0.2
 #define PID_D_HEATING 4
@@ -49,46 +49,16 @@
 #include <EEPROM.h>
 
 double temp, output, desired_temp;
-PID pid(&temp, &output, &desired_temp, PID_P, PID_I, PID_D, DIRECT);
-SoftwareSerial btSerial(BT_RX_PIN, BT_TX_PIN);
+double pid_p, pid_i, pid_d;
+PID pid(&temp, &output, &desired_temp, pid_p, pid_i, pid_d, DIRECT);
+SoftwareSerial bt_serial(BT_RX_PIN, BT_TX_PIN);
 boolean new_cycle;
 int cycles;
 int too_much_draw_counter;
 
-// For smoother fading
-const unsigned char cie[256] = {
-        0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 
-        1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 
-        2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 
-        3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 
-        5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 
-        7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 
-        10, 10, 11, 11, 11, 12, 12, 12, 13, 13, 
-        13, 14, 14, 15, 15, 15, 16, 16, 17, 17, 
-        17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 
-        22, 23, 23, 24, 24, 25, 25, 26, 26, 27, 
-        28, 28, 29, 29, 30, 31, 31, 32, 32, 33, 
-        34, 34, 35, 36, 37, 37, 38, 39, 39, 40, 
-        41, 42, 43, 43, 44, 45, 46, 47, 47, 48, 
-        49, 50, 51, 52, 53, 54, 54, 55, 56, 57, 
-        58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 
-        68, 70, 71, 72, 73, 74, 75, 76, 77, 79, 
-        80, 81, 82, 83, 85, 86, 87, 88, 90, 91, 
-        92, 94, 95, 96, 98, 99, 100, 102, 103, 105, 
-        106, 108, 109, 110, 112, 113, 115, 116, 118, 120, 
-        121, 123, 124, 126, 128, 129, 131, 132, 134, 136, 
-        138, 139, 141, 143, 145, 146, 148, 150, 152, 154, 
-        155, 157, 159, 161, 163, 165, 167, 169, 171, 173, 
-        175, 177, 179, 181, 183, 185, 187, 189, 191, 193, 
-        196, 198, 200, 202, 204, 207, 209, 211, 214, 216, 
-        218, 220, 223, 225, 228, 230, 232, 235, 237, 240, 
-        242, 245, 247, 250, 252, 255
-};
-
-
 void setup() {
     Serial.begin(9600);
-    btSerial.begin(9600);
+    bt_serial.begin(9600);
     Serial.println("#####################################");
     Serial.println("#####         VAPODUINO         #####");
     Serial.println("#####################################\n\n");
@@ -100,6 +70,10 @@ void setup() {
     pinMode(VIBRATOR_PIN, OUTPUT);
 
     desired_temp = get_desired_temp();
+
+    pid_p = PID_P;
+    pid_i = PID_I;
+    pid_d = PID_D;
     
     // Setting up max31865
     while (!max_init()) {
@@ -123,6 +97,10 @@ void setup() {
     
     new_cycle = true;
     too_much_draw_counter = 0;
+
+    digitalWrite(LED_PIN, HIGH);
+    vibrate_for_ms(500);
+    digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
@@ -139,7 +117,7 @@ void loop() {
             heatUpChamber();
             
             new_cycle = false;
-            pid = PID(&temp, &output, &desired_temp, PID_P, PID_I, PID_D, DIRECT);
+            pid = PID(&temp, &output, &desired_temp, pid_p, pid_i, pid_d, DIRECT);
             pid.SetOutputLimits(0, 255);
             pid.SetMode(AUTOMATIC);
         }
@@ -209,6 +187,12 @@ void printStatus() {
     int raw = analogRead(A3);
     Serial.print((int) desired_temp);
     Serial.print(";");
+    Serial.print(pid_p);
+    Serial.print(";");
+    Serial.print(pid_i);
+    Serial.print(";");
+    Serial.print(pid_d);
+    Serial.print(";");
     Serial.print(temp);
     Serial.print(";");
     Serial.print(output);
@@ -217,15 +201,15 @@ void printStatus() {
     Serial.print(";");
     Serial.println(get_battery_percents(raw * VOLTAGE_DIVIDER));
     
-    btSerial.print((int) desired_temp);
-    btSerial.print(";");
-    btSerial.print(temp);
-    btSerial.print(";");
-    btSerial.print(output);
-    btSerial.print(";");
-    btSerial.print(raw * VOLTAGE_DIVIDER);
-    btSerial.print(";");
-    btSerial.println(get_battery_percents(raw * VOLTAGE_DIVIDER));
+    bt_serial.print((int) desired_temp);
+    bt_serial.print(";");
+    bt_serial.print(temp);
+    bt_serial.print(";");
+    bt_serial.print(output);
+    bt_serial.print(";");
+    bt_serial.print(raw * VOLTAGE_DIVIDER);
+    bt_serial.print(";");
+    bt_serial.println(get_battery_percents(raw * VOLTAGE_DIVIDER));
 }
 
 void error(String message) {
@@ -234,18 +218,43 @@ void error(String message) {
 }
 
 void check_serial() {
-    if (btSerial.available()) {
+    if (bt_serial.available()) {
         digitalWrite(MOSFET_GATE_PIN, LOW);
-        char command = btSerial.read();
+        char command = bt_serial.read();
         byte value = 0;
         switch (command) {
             case 't':
-                while (!btSerial.available());
-                set_desired_temp(btSerial.read());
+                while (!bt_serial.available());
+                set_desired_temp(bt_serial.read());
                 break;
             default:
-                btSerial.println("Unknown Command");
+                bt_serial.println("Unknown Command");
         }
+    }
+
+    if (Serial.available()) {
+        digitalWrite(MOSFET_GATE_PIN, LOW);
+        if (Serial.read() == 'c') {
+            check_PID();
+        }
+    }
+}
+
+void check_PID() {
+    while (!Serial.available());
+    switch(Serial.read()) {
+        case 'p':
+            while (!Serial.available());
+            pid_p = ((float) Serial.read()) / 10;
+            break;
+        case 'i':
+            while (!Serial.available());
+            pid_i = ((float) Serial.read()) / 10;
+            break;
+        case 'd':
+            while (!Serial.available());
+            pid_d = ((float) Serial.read()) / 10;
+            break;
     }
 }
 
